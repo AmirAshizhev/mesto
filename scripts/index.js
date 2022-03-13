a882c0fe873a78a6f842433ef4e4d8fa9e3ff4e2
Amir Ashizhev <amirashizhev@yandex.ru>
2022-03-13 13:51:04 +0300

    feat: js

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const popup = document.querySelector('.popup');
 const openPopup = document.querySelector('.profile__edit-button');
 const closePopup = popup.querySelector('.popup__exit-button');
+const savePopup = popup.querySelector('.popup__save-button');
 
 function togglePopup () {
   popup.classList.toggle('popup_opened');
@@ -13,5 +14,30 @@ popup.addEventListener('click', function(event) {
 });
 
 openPopup.addEventListener('click', togglePopup);
-
 closePopup.addEventListener('click', togglePopup);
+
+
+
+let formElement = document.querySelector('.popup');
+
+let nameInput = popup.querySelector('.popup__name');
+let jobInput = popup.querySelector('.popup__description');
+
+
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+
+  let nameInputValue = nameInput.value;
+  let jobInputValue = jobInput.value;
+
+  let profileTitle = document.querySelector('.profile__title');
+  let profileSubtitle = document.querySelector('.profile__subtitle');
+
+  profileTitle.textContent = nameInputValue;
+  profileSubtitle.textContent = jobInputValue;
+  togglePopup();
+}
+
+
+popup.addEventListener('submit', formSubmitHandler);
