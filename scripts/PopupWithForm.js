d030866f0def40cf2aa1c89e8aaea316abed1112
Amir Ashizhev <amirashizhev@yandex.ru>
2022-05-19 06:14:28 +0300

    feat: class PopupWithForm begining

--- scripts/PopupWithForm.js
+++ scripts/PopupWithForm.js
@@ -1,9 +1,24 @@
 import {Popup} from './Popup'
 
 class PopupWithForm extends Popup {
-  constructor(){
-
+  constructor(popup, handleFormSubmit){
+    super(popup);
+    this._handleFormSubmit = handleFormSubmit;
+    this._popupForm = this._popup.querySelector('.popup__form');
+    this._inputName = this._popup.querySelector('.popup__item_name');
+    this._inputDescription = this._popup.querySelector('.popup__item_description');
   }
+
+  _getInputValue(){
+    return this._inputName,
+    this._inputDescription
+  }
+
+  setEventListeners(){
+    super.setEventListeners();
+    this._popupForm.addEventListener('submit', this._handleFormSubmit);
+  }
+
 
 }
 
